06c7df63271833258a92c37c153c75df1d44b30e
lucas felix bastos <felixbastos.lucas@gmail.com>
2021-03-16 23:38:46 -0300

    correção metodo patch

--- src/main/java/com/tcc/resource/UsuarioResource.java
+++ src/main/java/com/tcc/resource/UsuarioResource.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -58,7 +57,7 @@ public class UsuarioResource {
 		return ResponseEntity.ok(retorno);
 	}
 	
-	@PatchMapping("/password")
+	@PutMapping("/password")
 	public ResponseEntity<Void> updatePassword(@RequestBody UpdatePasswordFormDTO dto){
 		this.userService.updatePassword(dto);
 		return ResponseEntity.noContent().build();
